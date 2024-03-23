0f52486f49b7df3d631309faa7543ebf445b6feb
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-23 14:59:57 +0800

    Add keyof constraint lecture and update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 //import builtInGenerics from "./lectures/built-in-generics";
 //import genericFunction from "./lectures/generic-function";
 //import constraints from "./lectures/constraints";
-import anoherGenericFunction from "./lectures/another-generic-function";
+//import anoherGenericFunction from "./lectures/another-generic-function";
+import keyofConstraint from "./lectures/keyof-constraint";
 
-export default function app() {  
-  anoherGenericFunction();
+export default function app() {    
+  keyofConstraint();
 } 
