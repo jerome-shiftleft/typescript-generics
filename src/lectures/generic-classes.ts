619bd0526466b4ec379e7ca294e7c79881ea7edc
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-23 15:10:46 +0800

    Add generic classes and methods

--- src/lectures/generic-classes.ts
+++ src/lectures/generic-classes.ts
@@ -1,1 +1,57 @@
-export default function genericClasses() {}
+export default function genericClasses() {
+  function merge<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+  }
+
+  const mergeObj = merge({ name: "Alice", hobbies: ["sports"] }, { age: 30 });
+  console.log(mergeObj);
+
+  interface Lengthy {
+    length: number;
+  }
+
+  function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = "Got no value.";
+    if (element.length === 1) {
+      descriptionText = `Got 1 element`;
+    } else if (element.length > 1) {
+      descriptionText = `Got ${element.length} elements`;
+    }
+    return [element, descriptionText];
+  }
+
+  console.log(countAndDescribe("Hello World"));
+  //console.log(countAndDescribe(['sports', 'cooking']))
+
+  function extractAndConvert<T extends object, U extends keyof T>(
+    obj: T,
+    key: U
+  ) {
+    return obj[key];
+  }
+
+  console.log(extractAndConvert(mergeObj, "name"));
+
+  class DataStorage<T> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+      this.data.push(item);
+    }
+
+    removeItem(item: T) {
+      this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+      return [...this.data];
+    }
+  } // end of class Storage
+
+  const textStorage = new DataStorage<string>();
+  textStorage.addItem('Max');
+  textStorage.addItem('Manu');  
+  textStorage.removeItem('Max');
+  console.log(textStorage.getItems());
+
+} // end of export default function genericClasses()
