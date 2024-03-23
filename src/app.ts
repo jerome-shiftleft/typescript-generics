3d13d1268bc8366fde08193f865bbca9a7081f7f
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-23 15:38:27 +0800

    generic utility types starting setup

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,9 @@
 //import constraints from "./lectures/constraints";
 //import anoherGenericFunction from "./lectures/another-generic-function";
 //import keyofConstraint from "./lectures/keyof-constraint";
-import genericClasses from "./lectures/generic-classes";
+//import genericClasses from "./lectures/generic-classes";
+import genericUtilityTypes from "./lectures/generic-utility-types";
 
-export default function app() {
-  genericClasses();
+export default function app() {  
+  genericUtilityTypes()
 } 
